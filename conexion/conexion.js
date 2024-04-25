f057fa939eefd6eb176fba9bc761d9313131c76d
KEENTAA <165042155+KEENTAA@users.noreply.github.com>
2024-04-24 21:35:41 -0400

    Update conexion.js

--- conexion/conexion.js
+++ conexion/conexion.js
@@ -1,1 +1,19 @@
 
+const mongoose = require('mongoose');
+
+async function conectarBaseDatos() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/adminstradores');
+        console.log('Conexión exitosa a la base de datos');
+    } catch (error) {
+        console.error('Error al conectar a la base de datos:', error);
+    }
+}
+
+conectarBaseDatos(); 
+
+
+console.log('Intentando conectar a la base de datos...');
+
+
+module.exports = conectarBaseDatos;

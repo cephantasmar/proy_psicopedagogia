0b79f15184a9cfd7832fd59d52245fba0a0e1062
MarquezAlan <160274481+MarquezAlan@users.noreply.github.com>
2024-04-30 11:40:52 -0400

    Create DetalleMateria1.vue

---
+++ Malla_Curricular/src/components/DetalleMateria1.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="detalle-materia">
+      <h3>Psicopedagogia</h3>
+      <p>Aquí puedes encontrar información detallada sobre la Materia 1 INtroduccion a la Psicopedagogia.</p>
+      <p>Esta es una materia muy importante para el primer semestre y cubre temas como puntuales e introductorios al campo donde se desarrolla la carrera en general</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'DetalleMateria1'
+  };
+  </script>
+  
+  <style scoped>
+  .detalle-materia {
+    background-color: #ffffff;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+    padding: 20px;
+    max-width: 400px;
+    text-align: left;
+    font-size: 14px;
+  }
+  </style>
+  

df3012e077fecca1cfcac826f46e3633132658c6
JeanMarcoFernandez <165040684+JeanMarcoFernandez@users.noreply.github.com>
2024-05-02 02:32:58 -0400

    Update ACCIONZA.vue

--- pre_presentación/src/components/ACCIONZA.vue
+++ pre_presentación/src/components/ACCIONZA.vue
@@ -10,14 +10,14 @@
                 Esto incluye la evaluación de técnicas como el aprendizaje basado en proyectos, 
                 el aprendizaje colaborativo y el uso de la tecnología en el aula.
             </li>
-            <img class="banner" id="bannersoc1" src="../assets/logo.png">
+            <img class="banner" id="bannersoc1" src="../assets/ZASC/img15.jpg">
         </a>
         <a class="nav-item right">Análisis del Desarrollo Cognitivo y del Aprendizaje:
             <li>
                 Se han realizado investigaciones para comprender el desarrollo cognitivo y el proceso de aprendizaje en diferentes etapas de la vida. 
                 Estos estudios han proporcionado información valiosa sobre cómo diseñar programas educativos más efectivos y adaptados a las necesidades individuales de los estudiantes.
             </li>
-            <img class="banner" id="bannersoc1" src="../assets/logo.png">
+            <img class="banner" id="bannersoc1" src="../assets/ZASC/img10.jpg">
           </a>
     </nav>
   </template>
@@ -71,6 +71,7 @@
         width: 100%; 
         max-width: 800px; 
         text-align: left; 
+        position: relative;
     }
 
     .navigation .nav-item {
@@ -88,5 +89,10 @@
     .nav-item:hover {
         background-color: #86a029;
     }
-
+    .banner {
+        max-width: 500px; 
+        height: auto;
+        display: block; 
+        margin: 0 auto; 
+    }
 </style>
